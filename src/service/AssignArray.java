2244bffa2adb41c940cb80a9269d2b4e3c1e8104
wangwu <ww5201314>
2020-05-24 10:45:31 +0800

    我今天学习文件提交

--- src/service/AssignArray.java
+++ src/service/AssignArray.java
@@ -7,6 +7,7 @@ public class AssignArray {
                 System.out.println("book长度为"+book.length +"。book[0]="+book[0]);
                 book= new int[32];//book指向新的32的数组
                 System.out.println("book长度为"+book.length +"。book[0]="+book[0]);
+
                 System.out.println("book长度为"+book.length +"。book[0]="+book[0]);
 
 
