382b33e7e6a4dbce758f4d0f31a29166725c29d1
wangwu <wangwu@ngarihealth.com>
2020-06-04 09:54:33 +0800

    继承，接口实现，反射

--- src/service/wwtest/ColorTest.java
+++ src/service/wwtest/ColorTest.java
@@ -14,9 +14,9 @@ public class ColorTest {
 
     public static void  testDuibidu(ColorEntity color)throws Exception{
        //得到当前传进来color对象的getPrice方法
-        //写法一
-//        Class<? extends ColorEntity> clazz=color.getClass();
-        //写法二
+        //写法一,利用泛型写法
+        Class<? extends ColorEntity> clazz1=color.getClass();
+        //写法二,不利用泛型写法
         Class clazz=color.getClass();
         Method method=clazz.getMethod("getPrice",null);
         //获取到不同对象的 属性值来区分不同的实例对象
