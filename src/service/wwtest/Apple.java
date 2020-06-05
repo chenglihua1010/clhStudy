9ecb2b7e657cad2d56ede1ffada5b650e0be5435
wwclr <ww5201314>
2020-06-05 22:15:25 +0800

    继承，实现接口，多态

--- src/service/wwtest/Apple.java
+++ src/service/wwtest/Apple.java
@@ -2,6 +2,7 @@ package service.wwtest;
 
 //apple类实现了fruit接口,要么变成抽象类，要么实现全部方法
 public class Apple implements Fruit{
+
         @Override
         public void eat() {
                 System.out.println("苹果好吃");
@@ -13,6 +14,40 @@ public class Apple implements Fruit{
                 System.out.println("苹果好看");
         }
 
+        @Override
+        public void he(){
+                System.out.println("我是apple里面的hehe1");
+        }
+
+        @Override
+        public void he1() {
+
+        }
+
+        public static void main(String[] args) {
+                Fruit fruit=new Apple();
+                fruit.he();
+
+                Fruit fruit1=new Fruit() {
+                        @Override
+                        public void eat() {
+
+                        }
+
+                        @Override
+                        public void see() {
+
+                        }
+
+                        @Override
+                        public void he1() {
+
+                        }
+
+                };
+                fruit1.he();
+
+        }
 
 
 }
--- src/service/wwtest/Fruit.java
+++ src/service/wwtest/Fruit.java
@@ -3,4 +3,9 @@ package service.wwtest;
 public interface Fruit {
          void eat();
          void see();
+        default void he(){
+            System.out.println("我是 fruit里面的he");
+        }
+    void he1();
+
 }
